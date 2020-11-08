67b47e84142e53a22bce54562c172e8d1f586d20
Niklas Mueller <n2.muller@student.vu.nl>
2020-11-08 11:31:09 +0100

    Included gestures into execution of storyparts. Added comments

--- story.py
+++ story.py
@@ -1,26 +1,64 @@
 class Story:
-
+    """
+    Represents an possibly interaction story including parts, that are only told by the robots and parts that require the human to give answers.
+    """
     def __init__(self):
+        """
+        :return:
+        """
         self.story = [
-            {"id": "q1", "type": "question", "content": ("What is your name?", "answer_name")}, 
-            {"id": "q2", "type": "question", "content": ("How old are you?", "answer_age")}, 
-            {"id": "q3", "type": "question", "content": ("Do you want to hear a cool story?", "answer_decision")},
-            {"id": "p1", "type": "storypart", "content": "Cool {0}, since you are {1} years older than me, you probably know more than I do."}
+            Storypart("q1", "question", ("What is your name?", "answer_name")), 
+            Storypart("q2", "question", ("How old are you?", "answer_age")),
+            Storypart("q3", "question", ("Do you want to hear a cool story?", "answer_decision")),
+            Storypart("p1", "storypart", "Cool {0}, since you are {1} years older than me, you probably know more than I do.")
         ]
 
-
-    def __iter__(self):
+    def __iter__(self) -> StoryIterator:
+        """
+        :return: Interator object
+        """
         return StoryIterator(self.story)
+    
+class Storypart:
+    """
+    Represents a part of a story (e.g. a question or just text) that can include, text or gestures and can require human feedback.
+    """
+    def __init__(self, id: str, content_type: str, content, gesture: str = None):
+        """
+        :param id: Identification of storypart (str)
+        :param content_type: Determines whether human feedback is needed or not. (str)
+        :param content: Contains text and depending on the content_type also the dialogflow intent
+        :param gesture: Gesture to be executed during speech (str)
+        """
+        self.id = id
+        self.type = content_type
+        self.content = content
+        self.gesture = gesture
 
-    def format(self, text, user_model, story_part):
-        if story_part == "p1":
+    def hasGesture(self) -> bool:
+        """
+        Check whether a gesture is registered with this storypart.
+        :return: True, if a gesture has been registered (bool)
+        """
+        return self.gesture is not None
+
+    @staticmethod
+    def format(text, user_model, story_part_id) -> str:
+        """
+        Formats a given text for the specified storypart and fills it with information from the user model.
+        :param text: Text to be formatted (str)
+        :param user_model: Model of the current user to retrieve the information from.
+        :param story_part_id: ID of the storypart, used to determine which information need to be filled in.
+        :return: Formatted string (str)
+        """
+        if story_part_id == "p1":
             return text.format(user_model["name"], str(int(user_model["age"] - 5)))
         else:
             return ""
 
 class StoryIterator():
     '''
-    Iterator class
+    Iterator class for a story
     '''
     def __init__(self, story):
         self.story = story
--- tell_story.py
+++ tell_story.py
@@ -1,36 +1,57 @@
-from conversation import Conversation
-from story import Story
-from social_interaction_cloud.abstract_connector import AbstractSICConnector
-from social_interaction_cloud.action import ActionRunner
+from conversation import Conversation, ReturnType
 from social_interaction_cloud.basic_connector import BasicSICConnector
-from social_interaction_cloud.detection_result_pb2 import DetectionResult
+from story import Story
+
 
 class Storyteller:
+    """
+    Represents the logic guiding conversations between a robot and a human.
+    """
+
     def __init__(self, server_ip, dialogflow_key_file, dialogflow_agent_id):
-        self.sic = BasicSICConnector(server_ip, 'en-US', dialogflow_key_file, dialogflow_agent_id)
+        """
+        :param server_ip: IP address of Social Interaction Cloud server
+        :param dialogflow_key_file: path to Google's Dialogflow key file (JSON)
+        :param dialogflow_agent_id: ID number of Dialogflow agent to be used (project ID)
+        """
+        self.sic = BasicSICConnector(
+            server_ip, 'en-US', dialogflow_key_file, dialogflow_agent_id)
         self.conversation = Conversation(self.sic)
         self.story = Story()
 
-    def run(self):
+    def run(self) -> None:
+        """
+        Start the social interaction cloud and a conversation.
+        :return:
+        """
         self.sic.start()
-        self.conversation.introduce()        
+        self.conversation.introduce()
 
         for part in self.story:
-            if part["type"] == "question":
-                question, intent = part["content"]
+            if part.content_type == "question":
+                question, intent = part.content
                 res = self.conversation.ask_question(question, intent)
-                if res == -1:
+                if res == ReturnType.STOP:
                     break
-            elif part["type"] == "storypart":
-                storypart = part["content"]
-                self.conversation.tell_story_part(self.story.format(storypart, self.conversation.user_model, part["id"]))
+                elif res == ReturnType.MAX_ATTEMPTS:
+                    self.conversation.tell_story_part(
+                        "Sorry, I did not understand your answer. To repeat the last part fistbump my right fist. To stop fistbump my left fist.")
+                    # ask for repetetion or ending through fist bump
+                    res = self.conversation.ask_question(question, intent)
+                    if res != ReturnType.SUCCESS:
+                        break
+
+            elif part.content_type == "storypart":
+                storypart = part.content
+                self.conversation.tell_story_part(Story.format(
+                    storypart, self.conversation.user_model, part.id))
 
         self.conversation.end_conversation()
         self.sic.stop()
 
-    
 
-storyteller = Storyteller('127.0.0.1',
-                  'newagent-tfdt-2585f6cb1ae5.json',
-                  'newagent-tfdt')
-storyteller.run()
+if __name__ == "__main__":
+    storyteller = Storyteller('127.0.0.1',
+                              'newagent-tfdt-2585f6cb1ae5.json',
+                              'newagent-tfdt')
+    storyteller.run()
